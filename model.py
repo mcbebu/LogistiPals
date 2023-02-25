d483be4a00c3f4452f4deceb2be021efde84df7d
kwangyy <chia.kwang.yang@gmail.com>
2023-02-25 18:22:29 +0800

    updated + improved dimens

--- model.py
+++ model.py
@@ -6,119 +6,127 @@ import numpy as np
 import argparse
 import imutils
 import cv2
+
+
 def midpoint(ptA, ptB):
 	return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-	help="path to the input image")
 
-width = 5.8 
-
-args = vars(ap.parse_args())
-image = cv2.imread(args["image"])
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-gray = cv2.GaussianBlur(gray, (7, 7), 0)
+def max_dim():
 
 
-# perform edge detection, then perform a dilation + erosion to
-# close gaps in between object edges
-edged = cv2.Canny(gray, 50, 100)
-edged = cv2.dilate(edged, None, iterations=1)
-edged = cv2.erode(edged, None, iterations=1)
+	# construct the argument parse and parse the arguments
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-i", "--image", required=True,
+					help="path to the input image")
 
-# find contours in the edge map
-contours = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-contours = imutils.grab_contours(contours)
+	width = 5.8
 
-# sort the contours from left-to-right and initialize the
-# 'pixels per metric' calibration variable
-(contours, _) = contours.sort_contours(contours)
-pixels_per_metric = None
+	args = vars(ap.parse_args())
+	image = cv2.imread(args["image"])
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	gray = cv2.GaussianBlur(gray, (7, 7), 0)
 
-max_dim = None
-max_dim_total = 0
 
-# loop over the contours individually
-for c in contours:
-	# if the contour is not sufficiently large, ignore it
-	if cv2.contourArea(c) < 150:
-		continue
-		
+	# perform edge detection, then perform a dilation + erosion to
+	# close gaps in between object edges
+	edged = cv2.Canny(gray, 50, 100)
+	edged = cv2.dilate(edged, None, iterations=1)
+	edged = cv2.erode(edged, None, iterations=1)
+
+	# find contours in the edge map
+	cnts = cv2.findContours(
+		edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	cnts = imutils.grab_contours(cnts)
+
+	# sort the contours from left-to-right and initialize the
+	# 'pixels per metric' calibration variable
+	(cnts, _) = contours.sort_contours(cnts)
+	pixels_per_metric = None
+
+	max_dim = None
+	max_dim_total = 0
+
+	# loop over the contours individually
+	for c in cnts:
+		# if the contour is not sufficiently large, ignore it
+		if cv2.contourArea(c) < 150:
+			continue
+
 	# compute the rotated bounding box of the contour
-	orig = image.copy()
-	box = cv2.minAreaRect(c)
-	box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
-	box = np.array(box, dtype="int")
+		orig = image.copy()
+		box = cv2.minAreaRect(c)
+		box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
+		box = np.array(box, dtype="int")
 
-	# order the points in the contour such that they appear
-	# in top-left, top-right, bottom-right, and bottom-left
-	# order, then draw the outline of the rotated bounding box
-	box = perspective.order_points(box)
-	cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
+		# order the points in the contour such that they appear
+		# in top-left, top-right, bottom-right, and bottom-left
+		# order, then draw the outline of the rotated bounding box
+		box = perspective.order_points(box)
+		cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
 
-	# loop over the original points and draw them
-	for (x, y) in box:
-		cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
-		
-    # unpack the ordered bounding box, then compute the midpoint
-	# between the top-left and top-right coordinates, followed by
-	# the midpoint between bottom-left and bottom-right coordinates
-	(tl, tr, br, bl) = box
-	(tltrX, tltrY) = midpoint(tl, tr)
-	(blbrX, blbrY) = midpoint(bl, br)
+		# loop over the original points and draw them
+		for (x, y) in box:
+			cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
 
-	# compute the midpoint between the top-left and top-right points,
-	# followed by the midpoint between the top-righ and bottom-right
-	(tlblX, tlblY) = midpoint(tl, bl)
-	(trbrX, trbrY) = midpoint(tr, br)
+		# unpack the ordered bounding box, then compute the midpoint
+		# between the top-left and top-right coordinates, followed by
+		# the midpoint between bottom-left and bottom-right coordinates
+		(tl, tr, br, bl) = box
+		(tltrX, tltrY) = midpoint(tl, tr)
+		(blbrX, blbrY) = midpoint(bl, br)
 
-	# draw the midpoints on the image
-	cv2.circle(orig, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
-	cv2.circle(orig, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
-	cv2.circle(orig, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
-	cv2.circle(orig, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
+		# compute the midpoint between the top-left and top-right points,
+		# followed by the midpoint between the top-righ and bottom-right
+		(tlblX, tlblY) = midpoint(tl, bl)
+		(trbrX, trbrY) = midpoint(tr, br)
 
-	# draw lines between the midpoints
-	cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)), (255, 0, 255), 2)
-	cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)), (255, 0, 255), 2)
+		# draw the midpoints on the image
+		cv2.circle(orig, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
+		cv2.circle(orig, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
+		cv2.circle(orig, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
+		cv2.circle(orig, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
 
-	# find the dimensions using the euclidean distances
-	dim_a = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
-	dim_b = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
+		# draw lines between the midpoints
+		cv2.line(orig, (int(tltrX), int(tltrY)),
+				(int(blbrX), int(blbrY)), (255, 0, 255), 2)
+		cv2.line(orig, (int(tlblX), int(tlblY)),
+				(int(trbrX), int(trbrY)), (255, 0, 255), 2)
 
-	if pixels_per_metric == None:
-		# try to configure such that shortest = standard measurement 
-		pixels_per_metric = dim_b/ args["width"]
-		
-	# if the pixels per metric has not been initialized, then
-	# compute it as the ratio of pixels to supplied metric
-	# (in this case, inches)
-	dim_one = dim_a / pixels_per_metric
-	dim_two = dim_b / pixels_per_metric
+		# find the dimensions using the euclidean distances
+		dim_a = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
+		dim_b = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
 
-	# failsafe in case that dim_two is bigger than dim_one 
-	if dim_two > dim_one:
-		dim_one, dim_two = dim_two, dim_one
+		if pixels_per_metric == None:
+			# try to configure such that shortest = standard measurement
+			dim_min = min(dim_a, dim_b)
+			pixels_per_metric = dim_min / width
 
-	# we want the dims of the biggest box to do the recommendation 
-	if dim_one + dim_two > max_dim_total:
-		max_dim = (dim_one, dim_two)
-		max_dim_total = dim_one + dim_two
+		# if the pixels per metric has not been initialized, then
+		# compute it as the ratio of pixels to supplied metric
+		# (in this case, inches)
+		dim_one = dim_a / pixels_per_metric
+		dim_two = dim_b / pixels_per_metric
 
-	# draw the object sizes on the image
+		# failsafe in case that dim_two is bigger than dim_one
+		if dim_two > dim_one:
+			dim_one, dim_two = dim_two, dim_one
 
-	cv2.putText(orig, "{:.1f}cm".format(dim_one),
-		(int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
-		0.65, (255, 255, 255), 2)
+		# we want the dims of the biggest box to do the recommendation
+		if dim_one + dim_two > max_dim_total:
+			max_dim_tup = (dim_one, dim_two)
+			max_dim_total = dim_one + dim_two
 
-	cv2.putText(orig, "{:.1f}cm".format(dim_two),
-		(int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
-		0.65, (255, 255, 255), 2)
+		# draw the object sizes on the image
+		cv2.putText(orig, "{:.1f}cm".format(dim_one),
+					(int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
+					0.65, (255, 255, 255), 2)
 
-	# show the output image
-	cv2.imshow("Image", orig)
-	cv2.waitKey(0)
+		cv2.putText(orig, "{:.1f}cm".format(dim_two),
+					(int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
+					0.65, (255, 255, 255), 2)
 
-
+		# show the output image
+		cv2.imshow("Image", orig)
+		cv2.waitKey(0)
+	return max_dim_tup
